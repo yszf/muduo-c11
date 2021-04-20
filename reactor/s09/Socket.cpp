0230b87f2469e4cdbf9051978cdbe08e94209779
luobin <luobin_yszf@163.com>
2021-04-20 17:29:39 +0800

    reactor/s09 服务器忽略SIGPIPE，添加tcp nodelay和写完成回调

---
+++ reactor/s09/Socket.cpp
@@ -0,0 +1,48 @@
+#include "Socket.h"
+#include "SocketsOps.h"
+#include "InetAddress.h"
+
+#include <netinet/tcp.h>
+
+
+using namespace muduo;
+
+Socket::~Socket() {
+    sockets::close(sockfd_);
+}
+
+void Socket::bindAddress(const InetAddress& addr) {
+    sockets::bindOrDie(sockfd_, addr.getSockAddrInet());
+}
+
+void Socket::listen() {
+    sockets::listenOrDie(sockfd_);
+}
+
+int Socket::accept(InetAddress* peeraddr) {
+    struct sockaddr_in addr;
+    bzero(&addr, sizeof(addr));
+    int connfd = sockets::accept(sockfd_, &addr);
+    if (connfd >= 0) {
+        peeraddr->setSockAddrInet(addr);
+    }
+    return connfd;
+}
+
+void Socket::setReuseAddr(bool on) {
+    int optval = on ? 1 : 0;
+    int ret = ::setsockopt(sockfd_, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
+    assert(0 == ret);
+    (void) ret;
+}
+
+void Socket::shutdownWrite() {
+    sockets::shutdownWrite(sockfd_);
+}
+
+void Socket::setTcpNoDelay(bool on) {
+    int optval = on ? 1 : 0;
+    int ret = ::setsockopt(sockfd_, IPPROTO_TCP, TCP_NODELAY, &optval, sizeof optval);
+    assert(0 == ret);
+    (void) ret;
+}
